ec10a1c0dad7278e645d1612fd747b525969c06d
DNgo-Neumont <78094165+DNgo-Neumont@users.noreply.github.com>
2021-07-21 18:55:52 -0600

    Enough files for eclipse to work

---
+++ src/main/java/neumont/Main.java
@@ -0,0 +1,10 @@
+package neumont;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Is this enough of a project eclipse?");
+	}
+
+}
